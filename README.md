a1e72e2f5297ac660608f4d5e221165c11f6957b
Deejerz88 <djmcmillan93@gmail.com>
2022-06-26 09:32:49 -0400

    updated README, added comments

---
+++ README.md
@@ -0,0 +1,12 @@
+Don't fall behind on your busy schedule!
+
+Use the Plan On It Work Day Scheduler to keep track of your plans by the hour.
+
+Color-coded time blocks will tell you whether your plans are in the past (red), present (grey), or future (green)!
+
+Clicking the save icon will store that hour's plans.
+
+The Planner resets ever day.
+
+<img src='assets/images/Plan-on-it.png'>
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,15 @@ const currDayEl = $("#currentDay");
 const today = moment().format("LL");
 currDayEl.text(today);
 
-
+//get plans
 let plans = JSON.parse(localStorage.getItem("DailyPlanner"));
 if (!plans) {
   plans = {};
   plans[today] = {};
 }
 
+
+//create time blocks
 const createTimeblocks = () => {
   const timeblocks = $(".container");
   for (i = 9; i <= 17; i++) {
@@ -23,13 +25,14 @@ const createTimeblocks = () => {
       .before(`<div class="hour col pt-4">${hour}</div>`)
       .after('<button class="col btn saveBtn" id="save">💾</button>');
     
-    
+    //add plans to text area
      if (plans[today][hour]) $(`#${hour}`).children("textarea").val(plans[today][hour]);
   }
   checkTime();
   addListener()
 };
 
+//check if hour is in past, present, or future
 const checkTime = () => {
   $("textarea").each((i, el) => {
     const prev = $(el).prev();
@@ -45,6 +48,7 @@ const checkTime = () => {
   });
 };
 
+//save plans
 const savePlan = (e) => {
   const plan = $(e.target).prev()
   const planTime = $(plan).prev().text()
@@ -52,10 +56,13 @@ const savePlan = (e) => {
   localStorage.setItem('DailyPlanner', JSON.stringify(plans))
 }
 
+//add save button click event listener
 const addListener = () => {
    $("button").on("click", savePlan);
 }
 
+//check the time every minute
 setInterval(checkTime, 1000 * 60);
 
+//initiate time block creation
 $(window).on("load", createTimeblocks);
